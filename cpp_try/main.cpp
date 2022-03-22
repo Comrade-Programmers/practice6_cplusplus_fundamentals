14155bc500e8a8c0d1dc11543e4f45b04e378498
Opata Joshua <opatajoshua@gmail.com>
2022-03-22 20:34:50 +0000

    Initial Commit

---
+++ cpp_try/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cpp_try
+//
+//  Created by Joshua Opata on 22/03/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
